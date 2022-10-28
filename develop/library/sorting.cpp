a24c2cb1a8e2dc8b0ed6fbfecf0e91c837b28c44
misaka10032s <codyjj1012@gmail.com>
2022-10-28 18:55:03 +0800

    sorting

--- develop/library/sorting.cpp
+++ develop/library/sorting.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <math>
 
 #include "systemOS.h"
 #include "heap.h"
@@ -14,6 +15,7 @@ template <typename T> bool decreasing(T i0, T i1){
 };
 
 template <typename T> void quick_sort(T* start, int num, bool (*compare)(T i0, T i1)){
+    if(num <= 1) return;
     T datum = *start, tmp;
     int last = 0;
     for(int i=1; i<num - last; i++){
@@ -31,5 +33,40 @@ template <typename T> void quick_sort(T* start, int num, bool (*compare)(T i0, T
     quick_sort<T>(start, num - last - 1, compare);
     quick_sort<T>(start + num - last + 1, last - 1, compare);
 };
-template <typename T> void merge_sort(T* start, int num, bool (*compare)(T i0, T i1)){};
-template <typename T> void heap_sort(T* start, int num, bool (*compare)(T i0, T i1)){};
+
+// (0,1) (2,3) (4,5) (6,7)
+// (01,23) (45,67)
+// (0123, 4567)
+
+template <typename T> void merge_sort(T* start, int num, bool (*compare)(T i0, T i1)){
+    int range = ceil(log2(num)), iterA, iterB, loopTime, compareTme;
+    T *mirror = (T*)malloc(sizeof(T) * num), *tmp;
+    for(int i=0; i<range; i++){
+        loopTime = num/i/2;
+        for(int j=0; j<loopTime; j++){
+            iterA = j<<i;
+            iterB = (j+1)<<i;
+            compareTme = 1<<(i+1);
+            for(int k=0; k<compareTme; k++){
+                if(compare(*(start + iterA), *(start + iterB))){
+                    *(mirror + k) = *(start + iterA);
+                    iterA++;
+                }
+                else{
+                    *(mirror + k) = *(start + iterB);
+                    iterB++;
+                }
+            }
+        }
+        tmp = start;
+        start = mirror;
+        mirror = tmp;
+    }
+    if(range%2) for(int i=0; i<num; i++) *(start + i) = *(mirror + i);
+    free(range%2 ? start : mirror);
+};
+template <typename T> void heap_sort(T* start, int num, bool (*compare)(T i0, T i1)){
+    listHEAP<T> LH(compare);
+    for(int i=0; i<num; i++) LH.push(*(start + i));
+    for(int i=0; i<num; i++) *(start + i) = LH.pop();
+};
